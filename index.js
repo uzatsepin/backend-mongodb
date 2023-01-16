e369d1e9452187559f68f7213349b4ad43ce868e
Andrii Zatsepin <uzatsepin@gmail.com>
2023-01-16 22:34:03 +0200

    refactor: added UserController

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import express from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import { registerValitadion } from "./validations/auth.js";
-import { validationResult } from "express-validator";
-import UserModel from "./models/User.js";
 import checkAuth from "./utils/checkAuth.js";
+import * as UserController from "./controllers/UserController.js";
 
 mongoose.set("strictQuery", false);
 mongoose
@@ -20,98 +17,9 @@ mongoose
 const app = express();
 app.use(express.json());
 
-app.post("/login", async (req, res) => {
-  try {
-    const user = await UserModel.findOne({ email: req.body.email });
-    if (!user) {
-      return res.status(404).json({
-        message: "Некорректний логін або пароль",
-      });
-    }
-
-    const isValidPass = await bcrypt.compare(
-      req.body.password,
-      user._doc.passwordHash
-    );
-
-    if (!isValidPass) {
-      return res.status(400).json({
-        message: "Некорректний логін або пароль",
-      });
-    }
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      "secret123",
-      {
-        expiresIn: "30d",
-      }
-    );
-    const { passwordHash, ...userData } = user._doc;
-    res.json({
-      ...userData,
-      token,
-    });
-  } catch (err) {
-    res.status(500).json({
-      message: "Не вдалось авторизуватись",
-    });
-  }
-});
-
-app.post("/register", registerValitadion, async (req, res) => {
-  try {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array());
-    }
-
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-
-    const doc = new UserModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      passwordHash: hash,
-    });
-
-    const user = await doc.save();
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      "secret123",
-      {
-        expiresIn: "30d",
-      }
-    );
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({
-      ...userData,
-      token,
-    });
-  } catch (err) {
-    res.status(500).json({
-      message: "Реєстрація невдала",
-    });
-  }
-});
-
-app.get("/about", checkAuth, (req, res) => {
-  try {
-    res.json({
-      success: true,
-    });
-  } catch (err) {}
-});
+app.post("/login", UserController.login);
+app.post("/register", registerValitadion, UserController.register);
+app.get("/about", checkAuth, UserController.getAbout);
 
 app.listen(8080, (err) => {
   if (err) {
