307e1048ba98b3eff66aa4336213ffc2d18032d8
Andrii Zatsepin <uzatsepin@gmail.com>
2023-01-16 21:52:44 +0200

    feat: added hash, added token

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import UserModel from "./models/User.js";
 
 mongoose.set("strictQuery", false);
 mongoose
-  .connect("mongodb+srv://andriiadm:Pa3wtj1X@cluster0.wyiedjg.mongodb.net/test")
+  .connect("mongodb+srv://andriiadm:Pa3wtj1X@cluster0.wyiedjg.mongodb.net/blog")
   .then(() => {
     console.log("DB OK");
   })
@@ -20,26 +20,45 @@ const app = express();
 app.use(express.json());
 
 app.post("/register", registerValitadion, async (req, res) => {
-  const errors = validationResult(req);
+  try {
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    res.json({
+      ...user,
+      token,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Реєстрація невдала",
+    });
   }
-
-  const password = req.body.password;
-  const salt = await bcrypt.genSalt(10);
-  const passwordHash = await bcrypt.hash(password, salt);
-
-  const doc = new UserModel({
-    email: req.body.email,
-    fullName: req.body.fullName,
-    avatarUrl: req.body.avatarUrl,
-    passwordHash,
-  });
-
-  const user = await doc.save();
-
-  res.json(user);
 });
 
 app.listen(8080, (err) => {
