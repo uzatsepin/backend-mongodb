14f8d84ad7175382fb666697346c0a97d8e30170
Andrii Zatsepin <uzatsepin@gmail.com>
2023-01-16 21:43:28 +0200

    feat: added func to add user, everything ok

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import { registerValitadion } from "./validations/auth.js";
-import {validationResult} from "express-validator";
+import { validationResult } from "express-validator";
+import UserModel from "./models/User.js";
 
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb+srv://andriiadm:Pa3wtj1X@cluster0.wyiedjg.mongodb.net/test").then(() => {
-  console.log("DB OK");
-}).catch((error) => {
-  console.log("DB ERROR", error);
-});
+mongoose
+  .connect("mongodb+srv://andriiadm:Pa3wtj1X@cluster0.wyiedjg.mongodb.net/test")
+  .then(() => {
+    console.log("DB OK");
+  })
+  .catch((error) => {
+    console.log("DB ERROR", error);
+  });
 
 const app = express();
 app.use(express.json());
 
-app.post("/register", registerValitadion, (req, res) => {
+app.post("/register", registerValitadion, async (req, res) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array());
   }
 
-  res.json({
-    success: true,
-  })
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash,
+  });
+
+  const user = await doc.save();
+
+  res.json(user);
 });
 
 app.listen(8080, (err) => {
--- validations/auth.js
+++ validations/auth.js
@@ -3,6 +3,8 @@ import { body } from "express-validator";
 export const registerValitadion = [
   body("email", "Некорректно вказана пошта").isEmail(),
   body("password", "Пароль має бути довший 5 символів").isLength({ min: 5 }),
-  body("fullName", "Введіть корректне імʼя (довше 3 символів)").isLength({ min: 3 }),
-  body("avatarUrl", "Має бути саме лінк на аватар").isURL()
+  body("fullName", "Введіть корректне імʼя (довше 3 символів)").isLength({
+    min: 3,
+  }),
+  body("avatarUrl", "Має бути саме лінк на аватар").optional().isURL(),
 ];
